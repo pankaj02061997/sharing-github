bdbd760c115214c9b51d625ba84239db24f2435d
pankaj02061997 <57310271+pankaj02061997@users.noreply.github.com>
2019-11-03 11:27:59 +0530

    Add files via upload

---
+++ pankaj.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook 
+# 
+
+# **PANKAJ KUMAR NAHATA** 
+# 
+# I WANTED TO BECOME AN ACTUARY
+
+# _I am interested in data science because it uses mathematical and statistical calculations broudly and interpret the analytical results_
+
+# ### in next line it does an addition of 1+1 which gives 2###
+
+# In[1]:
+
+
+1+1
+
+
+# In[2]:
+
+
+x = "hello python"
+print(x)
+
+
+# In[ ]:
+
+
+
+
